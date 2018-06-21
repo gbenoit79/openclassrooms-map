8e58530b4aed70a9269794d35ee69e841fdf2232
Guillaume BENOIT <gbenoit79@gmail.com>
2018-06-21 19:20:37 +0200

    Display station informations

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,15 @@
 function initMap() {
     // Get map
     var lyonPosition = {lat: 45.75, lng: 4.85};
-    var map = new google.maps.Map(document.getElementById("map"), {
+    var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: lyonPosition
     });
 
     // Get stations
-    var url = "https://api.jcdecaux.com/vls/v1/stations";
-    var apiKey = "4046fd4b9fc4bb3fa4ef3f3c67efb9e2bbb41c25";
-    var contract = "Lyon";
+    var url = 'https://api.jcdecaux.com/vls/v1/stations';
+    var apiKey = '4046fd4b9fc4bb3fa4ef3f3c67efb9e2bbb41c25';
+    var contract = 'Lyon';
     $.ajax({
         url: url,
         data: {
@@ -17,17 +17,26 @@ function initMap() {
             apiKey: apiKey
         }
     })
-        .done(function( msg ) {
+        .done(function(msg) {
             // Add markers
-            $.each( msg, function( key, station ) {
+            $.each(msg, function(key, station) {
                 var marker = new google.maps.Marker({
                     position: station.position,
                     title: station.address,
                     map: map
                 });
+                marker.addListener('click', function() {
+                    $('#station').hide();
+                    var htmlString = 'Adresse: '+station.address+'<br /><br />';
+                    htmlString += station.bike_stands+' places<br />';
+                    htmlString += station.available_bikes+' vélos disponibles<br />';
+                    console.log(htmlString);
+                    $('#station-info').html(htmlString);
+                    $('#station').show();
+                });
             });
         })
-        .fail(function( jqXHR, textStatus ) {
+        .fail(function(jqXHR, textStatus) {
             console.log( "Request failed: " + textStatus );
         });
 }
